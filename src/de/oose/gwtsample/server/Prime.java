f6753e4fc4e44a625e59bb1aa252966defe9d7b7
Markus Klink <justjoheinz@gmail.com>
2012-11-06 10:14:36 +0100

    deferred version

--- src/de/oose/gwtsample/server/Prime.java
+++ src/de/oose/gwtsample/server/Prime.java
@@ -4,22 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Prime {
-	public static List<Integer> generatePrimes(int max)
-	{
-	    List<Integer> primes = new ArrayList<Integer>();
-	 
-	    // start from 2
-	    OUTERLOOP:
-	    for (int i = 2; i <= max; i++) {
-	        // try to divide it by all known primes
-	        for (Integer p : primes)
-	            if (i % p == 0)
-	                continue OUTERLOOP; // i is not prime
-	 
-	        // i is prime
-	        primes.add(i);
-	    }
-	    return primes;
+	public static List<Integer> generatePrimes(int max) {
+		List<Integer> primes = new ArrayList<Integer>();
+
+		// start from 2
+		OUTERLOOP: for (int i = 2; i <= max; i++) {
+			// try to divide it by all known primes
+			for (Integer p : primes)
+				if (i % p == 0)
+					continue OUTERLOOP; // i is not prime
+
+			// i is prime
+			primes.add(i);
+		}
+		return primes;
 	}
 
 }
