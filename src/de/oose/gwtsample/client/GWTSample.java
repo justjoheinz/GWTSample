9d65ce5ced9ffa9a344729a7260a8b6ebe9c8bc6
Markus Klink <justjoheinz@gmail.com>
2012-11-06 11:08:49 +0100

    new try

--- src/de/oose/gwtsample/client/GWTSample.java
+++ src/de/oose/gwtsample/client/GWTSample.java
@@ -14,12 +14,10 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 import de.oose.gwtsample.shared.Pair;
 
-
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GWTSample implements EntryPoint {
-	
 
 	private final PrimeServiceAsync primeService = GWT
 			.create(PrimeService.class);
@@ -37,61 +35,71 @@ public class GWTSample implements EntryPoint {
 
 		Element rootDiv = DOM.getElementById("primeContainer");
 		for (int i = 1; i <= 1000; i++) {
-			Element elem = DOM.createDiv();
-			elem.setInnerHTML(new Integer(i).toString());
-			elem.setId("prime" + new Integer(i).toString());
-			elem.setClassName("muted span1");
-			rootDiv.appendChild(elem);
-
-		}
-		
-		
-		class SendButtonHandler implements ClickHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				calculatePrimes();
-			}
-
-			private void calculatePrimes() {
-				
-				for (int i = 1000; i >= 1; i--) {
-					primeService.isPrime(i,
-							new AsyncCallback<Pair<Integer, Boolean>>() {
-
-								@Override
-								public void onFailure(Throwable caught) {
-									
-								}
-
-								@Override
-								public void onSuccess(
-										final Pair<Integer, Boolean> result) {
-									Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-										
-										@Override
-										public void execute() {
-											Element elem = DOM.getElementById("prime"
-													+ result.left);
-
-											if (result.right) {
-												elem.setClassName("text-success span1");
-											} else {
-												elem.setClassName("text-error span1");
-											}
-											
-										}
-									});
-								}
-							});
-				}
-
-			}
+			rootDiv.appendChild(createPrimeDiv(i));
 		}
 
 		// Add a handler to send the name to the server
 		SendButtonHandler handler = new SendButtonHandler();
 		sendButton.addClickHandler(handler);
+	}
+
+	class SendButtonHandler implements ClickHandler {
+		/**
+		 * Fired when the user clicks on the sendButton.
+		 */
+		public void onClick(ClickEvent event) {
+			calculatePrimes();
+		}
+
+	}
+
+	private void calculatePrimes() {
+
+		for (int i = 1000; i >= 1; i--) {
+			setStyleOfPrimeDiv(i, "muted span1");
+			final int prime = i;
+			Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+
+				@Override
+				public void execute() {
+					setStyleOfPrimeDiv(prime, "muted span1");
+					primeService.isPrime(prime,
+							new AsyncCallback<Pair<Integer, Boolean>>() {
+
+								@Override
+								public void onFailure(Throwable caught) {
+
+								}
+
+								@Override
+								public void onSuccess(
+										final Pair<Integer, Boolean> result) {
+
+									if (result.right) {
+										setStyleOfPrimeDiv(result.left,
+												"text-success span1");
+									} else {
+										setStyleOfPrimeDiv(result.left,
+												"text-error span1");
+									}
+
+								}
+							});
+				}
+			});
+		}
+	}
+
+	private void setStyleOfPrimeDiv(Integer prime, String style) {
+		Element elem = DOM.getElementById("prime" + prime);
+		elem.setClassName(style);
+	}
+
+	private Element createPrimeDiv(Integer prime) {
+		Element elem = DOM.createDiv();
+		elem.setInnerHTML(prime.toString());
+		elem.setId("prime" + prime);
+		elem.setClassName("muted span1");
+		return elem;
 	}
 }
